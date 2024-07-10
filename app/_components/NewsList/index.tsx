623a450c2a80dbecf09ccf1e1c2dfda5efecf34a
kodama takamasa <kota1986.0415@gmail.com>
2024-07-10 14:56:23 +0900

    4章まで完了

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./index.module.css";
 import Category from "../Category";
 import Date from "../Date";
@@ -14,7 +15,7 @@ export default function NewsList({ news }: Props) {
   }
   return news.map((article) => (
     <li key={article.id} className={styles.list}>
-      <div className={styles.link}>
+      <Link href={`/news/${article.id}`} className={styles.link}>
         <Image
           className={styles.image}
           src="/no-image.png"
@@ -29,7 +30,7 @@ export default function NewsList({ news }: Props) {
             <Date date={article.publishedAt ?? article.createdAt} />
           </dd>
         </dl>
-      </div>
+      </Link>
     </li>
   ));
 }
