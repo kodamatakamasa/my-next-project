015c8f0062a175eaf8bee7c6775245006f2efecd
kodama takamasa <kota1986.0415@gmail.com>
2024-07-12 10:01:23 +0900

    7章まで完了

--- app/_constants/index.tsx
+++ app/_constants/index.tsx
@@ -2,3 +2,4 @@ import exp from "constants";
 
 export const MEMBERS_LIST_LIMIT = 100;
 export const TOP_NEWS_LIMIT = 2;
+export const NEWS_LIST_LIMIT = 10;
--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -3,14 +3,20 @@ import { getNewsDetail } from "@/app/_libs/microcms";
 import Article from "@/app/_components/Article";
 import ButtonLink from "@/app/_components/ButtonLink";
 import styles from "./page.module.css";
+
 type Props = {
   params: {
     slug: string;
   };
+  searchParams: {
+    dk?: string;
+  };
 };
 
-export default async function Page({ params }: Props) {
-  const data = await getNewsDetail(params.slug).catch(notFound);
+export default async function Page({ params, searchParams }: Props) {
+  const data = await getNewsDetail(params.slug, {
+    draftKey: searchParams.dk,
+  }).catch(notFound);
   return (
     <>
       <Article data={data} />
